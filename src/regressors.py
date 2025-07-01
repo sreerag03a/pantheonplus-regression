b0f0a9dbffb02a507d64934f121efd4df8060bcd
sreerag03a <sreerag3ngu@gmail.com>
2025-07-01 12:29:08 +0530

    Added multiple linear regression

--- src/regressors.py
+++ src/regressors.py
@@ -1,15 +1,6 @@
 # import pandas as pd
 import numpy as np
-
-#Simple gradient descent linear regressor
-
-# class Regressor:
-#     def __init__(self, x , y, n_iter, learnrate = 0.05):
-#         self.learnrate = 0.05
-#         self.n_iterations = n_iter
-
-
-
+#Single feature linear regressor with gradient descent
 def linearregressor(x,y,n_iter, learnrate = 0.05):
     n_data = len(y)
     bias_term = np.c_[np.ones((n_data, 1)), x]
@@ -17,23 +8,51 @@ def linearregressor(x,y,n_iter, learnrate = 0.05):
     for i in range(n_iter):
         gradients = (1/n_data) * bias_term.T.dot(bias_term.dot(theta) - y)
         theta = theta - (learnrate*gradients)
-    return theta.flatten()
+    return theta.flatten(), bias_term #We export the bias term so that we can extract the predictions easily from the parameters as bias_term.dot(parameters)
 
-def thirdregressor(x,y,n_iter, learnrate = 0.05):
+#Linear regression with multiple features (multiple linear regression)
+def multi_linear(x,y,n_iter, learnrate = 0.05):
     n_data = len(y)
-    x_sq = x**2
-    x_cb = x**3
-    x_qd = x**4
-    x_pt = x**5
-    bias_term = np.c_[np.ones((n_data, 1)), x, x_sq, x_cb,x_qd, x_pt]
-    theta = np.random.randn(6,1)
+    
+    bias_term = np.c_[np.ones((n_data, 1)), x]
+    nlinear = bias_term.shape[1]
+    theta = np.random.randn(nlinear,1)
     for i in range(n_iter):
         gradients = (1/n_data) * bias_term.T.dot(bias_term.dot(theta) - y)
         theta = theta - (learnrate*gradients)
-    return theta.flatten()
+    return theta.flatten(),bias_term
 
-# x_data = np.random.rand(100,1)
-# y_data = 4*x_data + 12 + (0.25*np.random.randn(100, 1))
+#Polynomial regression with gradient descent
+def poly_regressor(x,y,n_iter, learnrate = 0.05, degree = 2):
+    n_data = len(y)
+    x_bias = np.empty((n_data,degree))
+    for i in range(degree):
+        n = i+1
+        x_bias[:,i] = x[:,0]**n
+    bias_term = np.concatenate((np.ones((n_data, 1)),x_bias), axis = 1)
+    npoly = bias_term.shape[1]
+    theta = np.random.randn(npoly,1)
+    for i in range(n_iter):
+        gradients = (1/n_data) * bias_term.T.dot(bias_term.dot(theta) - y)
+        theta = theta - (learnrate*gradients)
+    return theta.flatten(),bias_term
 
-# m,c = linearregressor(x_data,y_data,10000)
-# print(x_data)
+#Failed multiple feature polynomial regressor
+# def multi_poly(x,y,n_iter,learnrate = 0.05, degree = 2):
+#     n_data = len(y)
+#     xlen = x.shape[1]
+#     x_bias = np.empty((n_data,degree*xlen))
+#     for i in range(xlen):
+#         for j in range(degree):
+#             n = j + 1
+#             k = j*i
+#             x_bias[:,k] = x[:,i]**n
+#     bias_term = np.concatenate((np.ones((n_data, 1)),x_bias), axis = 1)
+#     npoly = bias_term.shape[1]
+#     theta = np.random.normal(0,100,size = (npoly,1))
+#     for i in range(n_iter):
+#         print(bias_term.dot(theta))
+#         gradients = (1/n_data) * bias_term.T.dot(bias_term.dot(theta) - y)
+#         theta = theta - (learnrate*gradients)
+#     return theta.flatten(),bias_term
+
